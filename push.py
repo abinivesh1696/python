0fcf616b0884027f3fd74200f14587283deba7fc
abinivesh1696 <abiniveshcsea@gmail.com>
2026-03-04 18:55:22 +0530

    changes

--- push.py
+++ push.py
@@ -2,14 +2,11 @@ print("sample for python")
 
 username = "ABHINIVESH"
 exp = 10
-is_active = True
 value = 12.000
 
 print("username",username)
 print("exp",exp)
-print("is_active",is_active)
 
 print(type (username))
 print(type (exp))
-print(type(is_active))
 print(type(value))
